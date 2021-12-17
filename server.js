6371cead158133d47a0170343c34dd11e2e27b66
alirezadalvand <alireza.d1339@gmail.com>
2021-12-17 00:12:19 -0800

    main rout add

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  console.log("main rout");
+  res.send("here is Home man");
+});
+
 app.get("/bitch", (req, res) => {
   console.log("Hi bitch");
   res.render("index", { text: "bastered" });
