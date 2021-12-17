d96b6ff5c521e8170f890db38e4dfe3730722e4b
alirezadalvand <alireza.d1339@gmail.com>
2021-12-16 23:33:54 -0800

    user Id added

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,8 @@ router.get("/Alireza", (req, res) => {
   res.send("I'm alireza dude");
 });
 
+router.get("/:id", (req, res) => {
+  res.send("it's one of Alirezas ..... alireza dlv");
+});
 
-
-
-module.exports = router 
+module.exports = router;
