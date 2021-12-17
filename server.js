873f90d2d98fd2295a4458e954b8a31c577213be
alirezadalvand <alireza.d1339@gmail.com>
2021-12-17 01:24:19 -0800

    server up with render

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   console.log("main rout");
-  res.send("here is Home man");
+  res.sendFile("./views/index.html", { root: __dirname });
 });
 
 app.get("/bitch", (req, res) => {
   console.log("Hi bitch");
-  res.render("index", { text: "bastered" });
 });
 
 const userRouter = require("./routes/users");
